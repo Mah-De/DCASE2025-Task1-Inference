793a1c8249a1d3d8c4507b1bb10389508f0780fd
MahD <mahdramezanee@gmail.com>
2025-06-27 17:26:14 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
 setup(
     name='Ramezanee-SUT-Task1',
     version='0.1.0',
-    description='Baseline Inference package',
-    author='Florian Schmid',
-    author_email="florian.schmid@jku.at",
+    description='SUT inference code',
+    author='Mohamad Mahdee Ramezanee',
+    author_email="mahdramezanee@gmail.com",
     packages=find_packages(),  # This auto-discovers the inner folder
     install_requires=requirements,
     include_package_data=True,
     package_data={
-        'Ramezanee-SUT-Task1': ["resources/*.wav", 'ckpts/*.ckpt', 'ckpts/*.*'],
+        'Ramezanee-SUT-Task1': ["resources/*.wav", 'ckpts/*.ckpt', 'ckpts/*.*],
     },
     python_requires='>=3.13',
 )
